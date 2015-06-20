c981dad3404abd5134ca3f52c8d756644bf5ddd4
M <lmiguelvargasf@gmail.com>
2015-06-20 17:32:03 -0500

    Refactor combined(read and send data) working at 100 ms as sampling time.

--- catkin_ws/src/ardrone_numeric_method_controller/scripts/example4_combined.py
+++ catkin_ws/src/ardrone_numeric_method_controller/scripts/example4_combined.py
@@ -3,10 +3,17 @@ import rospy
 from ardrone_autonomy.msg import Navdata
 from std_msgs.msg import Empty
 from geometry_msgs.msg import Twist, Vector3
+from util import flat_trim
+
+t = 0
+
 
 def callback(navdata):
-    t = navdata.header.stamp.to_sec()
-    print("received odometry message: time=%f battery=%f vx=%f vy=%f z=%f yaw=%f"%(t,navdata.batteryPercent,navdata.vx,navdata.vy,navdata.altd,navdata.rotZ))
+    global t
+    t = navdata
+    # t = navdata.header.stamp.to_sec()
+    # print("received odometry message: time=%f battery=%f vx=%f vy=%f z=%f yaw=%f"%(t,navdata.batteryPercent,navdata.vx,navdata.vy,navdata.altd,navdata.rotZ))
+
 
 if __name__ == '__main__':
     rospy.init_node('example_node', anonymous=True)
@@ -20,18 +27,29 @@ if __name__ == '__main__':
     pub_land = rospy.Publisher('/ardrone/land', Empty, queue_size=10)
     pub_reset = rospy.Publisher('/ardrone/reset', Empty, queue_size=10)
 
-    print("ready!")
     rospy.sleep(1.0)
+    print("ready!")
+    flat_trim()
 
     print("takeoff..")
     pub_takeoff.publish(Empty())
     rospy.sleep(5.0)
 
+    r = rospy.Rate(10)  # 10hz <=> 100 ms
+    i = 0
+    speed = 0.1
     while not rospy.is_shutdown():
-      # fly forward..
-      pub_velocity.publish(Twist(Vector3(0.1,0,0),Vector3(0,0,0)))
-      rospy.sleep(2.0)
+        pub_velocity.publish(Twist(Vector3(speed, 0, 0), Vector3(0, 0, 0)))
+        print("received odometry message for i = {0:d}: time={1:f} battery={2:f} vx={3:f} vy={4:f} z={5:f} yaw={6:f}"
+              .format(i, t.header.stamp.to_sec(), t.batteryPercent, t.vx, t.vy, t.altd, t.rotZ))
 
-      # fly backward..
-      pub_velocity.publish(Twist(Vector3(-0.1,0,0),Vector3(0,0,0)))
-      rospy.sleep(2.0)
+        i += 1
+
+        if i == 50:
+            speed *= -1
+        elif i >= 100:
+            break
+
+        r.sleep()
+
+    pub_land.publish(Empty())
