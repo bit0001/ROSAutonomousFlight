eb4724a06173cc7ca3f3581d3c6a16ec26fe7838
M <lmiguelvargasf@gmail.com>
2015-06-20 20:36:21 -0500

    Add referenceAndConstantFiles to gitignore.

---
+++ catkin_ws/src/ardrone_numeric_method_controller/scripts/main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+"""This is the main module of the project where the algorithm is executed."""
+
+_author__ = "L. Miguel Vargas F."
+__copyright__ = "Copyright 2015, National Polytechnic School, Ecuador"
+__credits__ = ["Mani Monajjemi Rob Knight", "Sika Abarca"]
+__license__ = "Noncommercial"
+__version__ = "1.0.0"
+__maintainer__ = "L. Miguel Vargas F."
+__email__ = "lmiguelvargasf@gmail.com"
+__status__ = "Development"
+
+
+from util import *
+
+
