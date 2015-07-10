54de4286cc6c5ca45019e618c8e27575c52edd60
M <lmiguelvargasf@gmail.com>
2015-07-10 08:20:48 -0500

    Add code to adjust array in the attached files.

--- catkin_ws/src/ardrone_numeric_method_controller/scripts/util.py
+++ catkin_ws/src/ardrone_numeric_method_controller/scripts/util.py
@@ -26,7 +26,28 @@ def save_attached_files_from_email(host, user, password, path_to_save_files):
 
     for attachment in last_received_email.attachments:
         with open(path_to_save_files + attachment[0], 'bw+') as f:
-            f.write(attachment[1])
+            MAX_LENGTH = 101
+            if "control_constants" not in attachment[0]:
+                string_attachment = attachment[1].decode(encoding='UTF-8')
+                list_attachment = string_attachment.split("\n")[:-1]
+
+                float_attachment_list = []
+
+                for e in list_attachment:
+                    float_attachment_list.append(float(e))
+
+                if len(float_attachment_list) <= MAX_LENGTH:
+                    print("The arrays remains the same.", len(float_attachment_list))
+                else:
+                    float_attachment_list = reduce_list_until_be_useful(float_attachment_list, MAX_LENGTH, 5)
+
+                string_to_save = ''
+                for item in float_attachment_list:
+                    string_to_save += str(item) + '\n'
+
+                f.write(string_to_save.encode(encoding='UTF-8'))
+            else:
+                f.write(attachment[1])
 
 def reduce_points(list_to_reduce, max_length):
     additional_points = len(list_to_reduce) - max_length
@@ -45,25 +66,27 @@ def reduce_list_until_be_useful(list_to_reduce, max_length, hysteresis):
     return list_to_reduce
 
 
-MAX_LENGTH = 101
-lengths = []
 
-for i in range(1000):
-    randomLength = random.randint(90, 200)
-    randomList = random.sample(range(1000), randomLength)
+if __name__ == "__main__":
+    MAX_LENGTH = 101
+    lengths = []
 
-    print("The length is " + str(randomLength))
+    for i in range(1000):
+        randomLength = random.randint(90, 200)
+        randomList = random.sample(range(1000), randomLength)
 
-    if len(randomList) <= MAX_LENGTH:
-        print("The arrays remains the same.", len(randomList))
-    else:
-        randomList = reduce_list_until_be_useful(randomList, MAX_LENGTH, 5)
+        print("The length is " + str(randomLength))
 
-    lengths.append(len(randomList))
-    print("The array has changed")
-    print("\t", "New length:", len(randomList))
-    lengths.append(len(randomList))
+        if len(randomList) <= MAX_LENGTH:
+            print("The arrays remains the same.", len(randomList))
+        else:
+            randomList = reduce_list_until_be_useful(randomList, MAX_LENGTH, 5)
 
-print("Lengths", lengths)
-print("Max length", max(lengths))
-print("Min Length", min(lengths))
+        lengths.append(len(randomList))
+        print("The array has changed")
+        print("\t", "New length:", len(randomList))
+        lengths.append(len(randomList))
+
+    print("Lengths", lengths)
+    print("Max length", max(lengths))
+    print("Min Length", min(lengths))
