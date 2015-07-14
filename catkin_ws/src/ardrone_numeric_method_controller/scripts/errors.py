d0bbed22e658cc7445ae65570d1f995eb8822e4c
M <lmiguelvargasf@gmail.com>
2015-07-13 23:54:23 -0500

    Add titles to axis and set leged for one graphic.

--- catkin_ws/src/ardrone_numeric_method_controller/scripts/errors.py
+++ catkin_ws/src/ardrone_numeric_method_controller/scripts/errors.py
@@ -4,3 +4,15 @@ This module contains the percentage of errors that have been computed using the 
 after a trajectory is completed.
 """
 
+from numpy import array
+from references import *
+from position import *
+
+
+def compute_percentage_error(approximate_list, exact_list):
+    return 100.0 * abs((array(approximate_list) - array(exact_list)) / array(exact_list))
+
+
+x_error = compute_percentage_error(x_n, x_ref_n)
+y_error = compute_percentage_error(y_n, y_ref_n)
+z_error = compute_percentage_error(z_n, z_ref_n)
--- catkin_ws/src/ardrone_numeric_method_controller/scripts/plot_results.py
+++ catkin_ws/src/ardrone_numeric_method_controller/scripts/plot_results.py
@@ -2,31 +2,62 @@
 """
 This is a script for plotting the results after following a trajectory.
 """
-import numpy as np
 import matplotlib.pyplot as plt
 from references import x_ref_n
 from references import y_ref_n
 from references import z_ref_n
-from references import get_float_list_from_txt_file
-from references import PATH_TO_REFERENCES
+from position import x_n
+from position import y_n
+from position import z_n
+from errors import x_error
+from errors import y_error
+from errors import z_error
 
-x_n = get_float_list_from_txt_file(PATH_TO_REFERENCES + "x_ref_n")
-y_n = get_float_list_from_txt_file(PATH_TO_REFERENCES + "y_ref_n")
-z_n = get_float_list_from_txt_file(PATH_TO_REFERENCES + "z_ref_n")
-t_n = get_float_list_from_txt_file(PATH_TO_REFERENCES + "x_ref_n")
 t_n = []
 
 for i in range(len(x_ref_n)):
     t_n.append(0.1 * i)
 
 figure, plot_array = plt.subplots(2, 3)
-plot_array[0, 0].plot(t_n, x_ref_n, 'r--', t_n, x_n, 'bs')
+figure.suptitle("References, Positions and Errors", fontsize=16)
+
+plot_array[0, 0].plot(t_n, x_n, 'b', label="Actual")
+plot_array[0, 0].plot(t_n, x_ref_n, 'r--', label="Reference")
 plot_array[0, 0].set_title('X-position vs. time')
-plot_array[0, 1].plot(t_n, y_ref_n, 'r--', t_n, y_n, 'bs')
+plot_array[0, 0].set_xlabel("Time [s]")
+plot_array[0, 0].set_ylabel("X-position [m]")
+legend = plot_array[0, 0].legend(loc="best", shadow=True)
+
+for label in legend.get_texts():
+    label.set_fontsize('medium')
+
+for label in legend.get_lines():
+    label.set_linewidth(1.0)
+
+
+plot_array[0, 1].plot(t_n, y_n, 'b', t_n, y_ref_n, 'r--')
 plot_array[0, 1].set_title('Y-position vs. time')
-plot_array[0, 2].plot(t_n, z_ref_n, 'r--', t_n, z_n, 'bs')
+plot_array[0, 1].set_xlabel("Time [s]")
+plot_array[0, 1].set_ylabel("Y-position [m]")
+
+plot_array[0, 2].plot(t_n, z_n, 'b', t_n, z_ref_n, 'r--')
 plot_array[0, 2].set_title('Z-Position vs. time')
+plot_array[0, 2].set_xlabel("Time [s]")
+plot_array[0, 2].set_ylabel("Z-position [m]")
+
+plot_array[1, 0].plot(t_n, x_error, 'r--')
 plot_array[1, 0].set_title('X-errors vs. time')
+plot_array[1, 0].set_xlabel("Time [s]")
+plot_array[1, 0].set_ylabel("Error %")
+
+plot_array[1, 1].plot(t_n, y_error, 'r--')
 plot_array[1, 1].set_title('Y-errors vs. time')
+plot_array[1, 1].set_xlabel("Time [s]")
+plot_array[1, 1].set_ylabel("Error %")
+
+plot_array[1, 2].plot(t_n, z_error, 'r--')
 plot_array[1, 2].set_title('Z-errors vs. time')
+plot_array[1, 2].set_xlabel("Time [s]")
+plot_array[1, 2].set_ylabel("Error %")
+
 plt.show()
