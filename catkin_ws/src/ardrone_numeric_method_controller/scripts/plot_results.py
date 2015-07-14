d2bb914df935b7e3a4f7f9271688bef32aab04ad
M <lmiguelvargasf@gmail.com>
2015-07-14 00:27:29 -0500

    Add functions show_legend and plot_reference_and_position to reuse code.

--- catkin_ws/src/ardrone_numeric_method_controller/scripts/plot_results.py
+++ catkin_ws/src/ardrone_numeric_method_controller/scripts/plot_results.py
@@ -13,37 +13,44 @@ from errors import x_error
 from errors import y_error
 from errors import z_error
 
-t_n = []
 
-for i in range(len(x_ref_n)):
-    t_n.append(0.1 * i)
+def show_legend(figure):
+    legend = figure.legend(loc="best", shadow=True)
 
-figure, plot_array = plt.subplots(2, 3)
-figure.suptitle("References, Positions and Errors", fontsize=16)
+    for label in legend.get_texts():
+        label.set_fontsize('medium')
 
-plot_array[0, 0].plot(t_n, x_n, 'b', label="Actual")
-plot_array[0, 0].plot(t_n, x_ref_n, 'r--', label="Reference")
+    for label in legend.get_lines():
+        label.set_linewidth(1.0)
+
+
+def plot_reference_and_position(figure, reference, position, time):
+    figure.plot(t_n, position, 'b', label="Actual")
+    figure.plot(t_n, reference, 'r--', label="Reference")
+
+
+t_n = [0.1 * i for i in range(len(x_ref_n))]
+
+whole_figure, plot_array = plt.subplots(2, 3)
+whole_figure.suptitle("References, Positions and Errors", fontsize=16)
+
+plot_reference_and_position(plot_array[0, 0], x_ref_n, x_n, t_n)
 plot_array[0, 0].set_title('X-position vs. time')
 plot_array[0, 0].set_xlabel("Time [s]")
 plot_array[0, 0].set_ylabel("X-position [m]")
-legend = plot_array[0, 0].legend(loc="best", shadow=True)
+show_legend(plot_array[0, 0])
 
-for label in legend.get_texts():
-    label.set_fontsize('medium')
-
-for label in legend.get_lines():
-    label.set_linewidth(1.0)
-
-
-plot_array[0, 1].plot(t_n, y_n, 'b', t_n, y_ref_n, 'r--')
+plot_reference_and_position(plot_array[0, 1], y_ref_n, y_n, t_n)
 plot_array[0, 1].set_title('Y-position vs. time')
 plot_array[0, 1].set_xlabel("Time [s]")
 plot_array[0, 1].set_ylabel("Y-position [m]")
+show_legend(plot_array[0, 1])
 
-plot_array[0, 2].plot(t_n, z_n, 'b', t_n, z_ref_n, 'r--')
+plot_reference_and_position(plot_array[0, 2], z_ref_n, z_n, t_n)
 plot_array[0, 2].set_title('Z-Position vs. time')
 plot_array[0, 2].set_xlabel("Time [s]")
 plot_array[0, 2].set_ylabel("Z-position [m]")
+show_legend(plot_array[0, 2])
 
 plot_array[1, 0].plot(t_n, x_error, 'r--')
 plot_array[1, 0].set_title('X-errors vs. time')
