e8702de803bb72b0d9cfeca5c18608b8ea0343cb
M <lmiguelvargasf@gmail.com>
2015-07-14 19:49:08 -0500

    Add button for drawing trajectory before following trajectory.

---
+++ catkin_ws/src/ardrone_numeric_method_controller/scripts/draw_trajectory.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""
+This is a script for plotting the trajectory that the drone is supposed to follow.
+"""
+print("Hello world!")
--- catkin_ws/src/ardrone_numeric_method_controller/scripts/ros_initial_interface.py
+++ catkin_ws/src/ardrone_numeric_method_controller/scripts/ros_initial_interface.py
@@ -11,43 +11,52 @@ import subprocess
 class ROSLauncher:
     def __init__(self):
         window = Tk()
+        window.title("Thesis Interface")
         window.resizable(width=FALSE, height=FALSE)
-        self.change_window_size(window, 500, 400)
+        self.change_window_size(window, 500, 350)
 
         top_frame = Frame(window)
         top_frame.pack()
-        below_top_frame = Frame(window)
-        below_top_frame.pack()
+        image_frame = Frame(window)
+        image_frame.pack()
         middle_frame = Frame(window)
         middle_frame.pack()
         bottom_frame = Frame(window)
         bottom_frame.pack()
 
-        window.title("Thesis Interface")
-
-        drone_image = PhotoImage(file='./ardrone.gif')
-        display_drone_image = drone_image.subsample(2, 2)
-
-        label = Label(below_top_frame, image=drone_image).pack(side=LEFT)
-
         title_label = Label(top_frame, text="GUI for ARDrone Autonomous Flight", font="bold")
         title_label.pack()
-        save_files_button = Button(middle_frame, text="Save attached files", command=self.get_files_from_email)
+
+        self.insert_spaces_in_frame(top_frame, 1)
+
+        drone_image = PhotoImage(file='./ardrone.gif')
+        image_label = Label(image_frame, image=drone_image)
+        image_label.pack(side=LEFT)
+
+        self.insert_spaces_in_frame(middle_frame, 1)
+
+        save_files_button = Button(middle_frame, text="Save Attached Files", command=self.get_files_from_email)
         save_files_button.pack(side=LEFT)
         ros_start_button = Button(middle_frame, text="Start ROS", command=self.start_ROS)
         ros_start_button.pack(side=LEFT)
-        follow_trajectory_button = Button(middle_frame, text="Follow Trajectory", command=self.follow_trajectory)
+        draw_trajectory_button = Button(middle_frame, text="Draw Trajectory", command=self.draw_trajectory)
+        draw_trajectory_button.pack(side=LEFT)
+        follow_trajectory_button = Button(bottom_frame, text="Follow Trajectory", command=self.follow_trajectory)
         follow_trajectory_button.pack(side=LEFT)
         plot_results_button = Button(bottom_frame, text="Plot Results", command=self.plot_results)
         plot_results_button.pack()
 
-        window.mainloop()  # Create an event loop
+
+        window.mainloop()
 
     def start_ROS(self):
         os.system("bash start_ROS.sh &")
 
     def get_files_from_email(self):
         subprocess.call("python3 save_references.py 1", shell=True)
+
+    def draw_trajectory(self):
+        subprocess.call("python3 draw_trajectory.py 1", shell=True)
 
     def follow_trajectory(self):
         subprocess.call("python3 main.py 1", shell=True)
@@ -57,6 +66,11 @@ class ROSLauncher:
 
     def change_window_size(self, window, width, height):
         window.geometry('{}x{}'.format(width, height))
+
+    def insert_spaces_in_frame(self, frame, spaces):
+        for i in range(spaces):
+            blank_label = Label(frame, text=" ")
+            blank_label.pack()
 
 
 ROSLauncher()
